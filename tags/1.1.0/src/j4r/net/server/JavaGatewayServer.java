4c3adf16da4a229d5a7c8fd2a8b1dd213aa320ce
donenrique <donenrique@eea6d730-789a-414e-8eaf-1ea38fa96e50>
2021-04-30 18:57:00 +0000

    Tag corresponding to J4R version 1.1.0

---
+++ tags/1.1.0/src/j4r/net/server/JavaGatewayServer.java
@@ -0,0 +1,255 @@
+/*
+ * This file is part of the j4r library.
+ *
+ * Copyright (C) 2020-2021 Her Majesty the Queen in right of Canada
+ * Author: Mathieu Fortin, Canadian Wood Fibre Centre, Canadian Forest Service.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package j4r.net.server;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.InetAddress;
+import java.util.concurrent.ConcurrentHashMap;
+
+import j4r.lang.codetranslator.REnvironment;
+import j4r.net.server.BasicClient.ClientRequest;
+
+/**
+ * The JavaLocalGatewayServer class is a server that makes it possible 
+ * to create objects and to execute methods in Java from a non-Java application.
+ * @author Mathieu Fortin 
+ */
+public class JavaGatewayServer extends AbstractServer {
+	
+	
+	public static final String EXTENSION = "-ext";
+	public static final String PORT = "-ports"; 
+	public static final String BACKDOORPORT = "-backdoorport";
+	public static final String NB_PORTS = "-nbports";
+	public static final String WD = "-wd";
+	public static final String MEMORY = "-mem";
+	public static final String PortSplitter = ":";
+	public static final String PUBLIC = "-public";	// TODO MF2021-04-29 change this for something else like -public
+	public static final String KEY = "-key";
+
+	
+	/**
+	 * A wrapper for Exception.
+	 * @author Mathieu Fortin
+	 */
+	final class JavaGatewayException extends Exception {
+		
+		final Throwable nestedException;
+		
+		JavaGatewayException(Throwable e) {
+			super(e);
+			this.nestedException = e;
+		}
+
+		
+		public String toString() {
+			return this.getClass().getName() + "_" + nestedException.toString();
+		}
+	}
+	
+
+	private class JavaGatewayClientThread extends ClientThread {
+
+		REnvironment translator;
+		
+		protected JavaGatewayClientThread(AbstractServer.CallReceiverThread receiver, int workerID) {
+			super(receiver, workerID);
+		}
+
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					firePropertyChange("status", null, "Waiting");
+					socketWrapper = receiver.clientQueue.take();
+					InetAddress clientAddress = socketWrapper.getInetAddress();
+					this.translator = registerClient(clientAddress);
+					JavaGatewayServer.this.whoIsWorkingForWho.put(this, clientAddress);
+					firePropertyChange("status", null, "Connected to client: " + clientAddress.getHostAddress());		
+					while (!socketWrapper.isClosed()) {
+						try {
+							Object somethingInParticular = processRequest();
+							if (somethingInParticular != null) {
+								if (Thread.interrupted()) {
+									throw new InterruptedException();
+								}
+								if (somethingInParticular.equals(BasicClient.ClientRequest.closeConnection) 
+										|| somethingInParticular.equals(BasicClient.ClientRequest.closeConnection.name())) {
+									socketWrapper.writeObject(ServerReply.ClosingConnection);
+									closeSocket();
+									JavaGatewayServer.this.whoIsWorkingForWho.remove(this);
+									if (!JavaGatewayServer.this.whoIsWorkingForWho.contains(clientAddress)) {
+										JavaGatewayServer.this.translators.remove(clientAddress);
+									}
+									if (JavaGatewayServer.this.configuration.isPrivateServer()) {
+										JavaGatewayServer.this.requestShutdown();
+									}
+									break;
+								} else {
+									socketWrapper.writeObject(somethingInParticular);
+								}
+							} else {
+								socketWrapper.writeObject(ServerReply.Done);
+							}
+						} catch (Exception e) {		// something wrong happened during the processing of the request
+							try {
+								if (e instanceof IOException) {	// seems that the connection was lost
+									closeSocket();
+								} else if (!socketWrapper.isClosed()) {
+									if (e instanceof InvocationTargetException) {
+										socketWrapper.writeObject(new JavaGatewayException(((InvocationTargetException) e).getTargetException()));
+									} else {
+										socketWrapper.writeObject(new JavaGatewayException(e));
+									}
+								}
+							} catch (IOException e1) {}
+						}
+					}
+					if (JavaGatewayServer.this.shutdownOnClosedConnection) {
+						JavaGatewayServer.this.requestShutdown();
+						break;
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+
+		@Override
+		protected Object processRequest() throws Exception {
+			Object crudeRequest = getSocket().readObject();
+			if (crudeRequest instanceof String) {
+				String request = (String) crudeRequest;
+				if (ClientRequest.closeConnection.name().equals(request.trim())) {
+					return ClientRequest.closeConnection;
+				} else {
+					if (request.startsWith("time")) {
+						long startMillisec = Long.parseLong(request.substring(4));
+						long finalTime = System.currentTimeMillis();
+						double elapsedTime =  (finalTime - startMillisec);
+						System.out.println("Elapsed time single received packet:" + elapsedTime);
+					}
+					return this.translator.processCode(request);
+				}
+			} else {
+				return null;
+			}
+		}
+
+	}
+
+	private static JavaGatewayServer Instance;
+	
+	protected final ConcurrentHashMap<InetAddress, REnvironment> translators;	
+	protected final boolean shutdownOnClosedConnection;
+	protected boolean bypassShutdownForTesting;	// TODO: MF2021-04-29 Might be deprecated
+	private final Object mainInstance;
+	
+	/**
+	 * Constructor.
+	 * @param servConf a ServerConfiguration instance
+	 * @param mainInstance an Object that can later be accessed by the client using the static getMainInstance() method
+	 * @throws Exception
+	 */
+	public JavaGatewayServer(ServerConfiguration servConf, Object mainInstance) throws Exception {
+		super(servConf, false);
+		this.translators = new ConcurrentHashMap<InetAddress, REnvironment>();
+		this.shutdownOnClosedConnection = servConf.isPrivateServer();	// enable shutdown on close connection for private servers only
+		this.mainInstance = mainInstance;
+		Instance = this;
+	}
+
+	/**
+	 * Provide the main instance. <br>
+	 * <br> 
+	 * This instance is set through the constructor and can be null if there is no need for such an instance.
+	 * @return an Object instance
+	 */
+	public static Object getMainInstance() {
+		return Instance.mainInstance;
+	}
+
+	synchronized REnvironment registerClient(InetAddress clientAddress) {
+		if (!translators.containsKey(clientAddress)) {
+			translators.put(clientAddress, new REnvironment());
+		}
+		return(translators.get(clientAddress));
+	}
+
+
+	
+	
+	
+//	/**
+//	 * Hidden constructor for test purpose.
+//	 * 
+//	 * @param servConf a ServerConfiguration instance
+//	 * @param translator an instance that implements the REpiceaCodeTranslator interface
+//	 * @param shutdownOnClosedConnection by default this parameter is set to true so that if the connection is lost, the server is shutdown.
+//	 * @throws Exception
+//	 */
+//	protected JavaGatewayServer(ServerConfiguration servConf, REnvironment translator, boolean shutdownOnClosedConnection) throws Exception {
+//		super(servConf, false);
+//		this.translators = new ConcurrentHashMap<InetAddress, REnvironment>();
+//		this.shutdownOnClosedConnection = shutdownOnClosedConnection;
+//	}
+
+	@Override
+	protected ClientThread createClientThread(AbstractServer.CallReceiverThread receiver, int id) {
+		return new JavaGatewayClientThread(receiver, id);
+	}
+
+	
+	@Override
+	protected void shutdown(int shutdownCode) {
+		if (backdoorThread.isAlive()) {
+			backdoorThread.softExit();
+		}
+		if (bypassShutdownForTesting) {
+			return;
+		}
+		super.shutdown(shutdownCode);
+	}
+
+	@Override
+	protected void createFileInfoForLocalServer() throws IOException {
+		String filename = getConfiguration().wd.trim() + File.separator + "J4RTmpFile";
+		File file = new File(filename);
+		String realizedListeningPorts = "";
+		for (CallReceiverThread t : callReceiverThreads) {
+			if (!realizedListeningPorts.isEmpty()) {
+				realizedListeningPorts += PortSplitter;
+			}
+			realizedListeningPorts += t.serverSocket.getLocalPort();
+		}
+		String outputStr = "" + getConfiguration().key + ";" + 
+				backdoorThread.emergencySocket.getLocalPort() + PortSplitter + gcReceiverThread.serverSocket.getLocalPort() + ";" + 
+				realizedListeningPorts; 
+		FileWriter writer = new FileWriter(file);
+		writer.write(outputStr);
+		writer.close();
+	}
+	
+	
+}
---
+++ tags/1.1.0/src/j4r/net/server/ServerConfiguration.java
@@ -0,0 +1,152 @@
+/*
+ * This file is part of the j4r library.
+ *
+ * Copyright (C) 2020-2021 Her Majesty the Queen in right of Canada
+ * Author: Mathieu Fortin, Canadian Wood Fibre Centre, Canadian Forest Service.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed with the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Please see the license at http://www.gnu.org/copyleft/lesser.html.
+ */
+package j4r.net.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerConfiguration implements Serializable {
+
+	private static final long serialVersionUID = 20111222L;
+	
+	private static final int MIN_PORT = 1024;
+	private static final int MAX_PORT = 65535;
+	
+	protected final int numberOfClientThreadsPerReceiver;
+	protected final int maxSizeOfWaitingList;
+	protected final int[] listiningPorts;
+	protected final int[] internalPorts;
+	private final boolean isPrivateServer;
+	protected final int key;
+	protected final String wd;
+
+	
+	/**
+	 * Constructor for public servers. 
+	 * 
+	 * @param numberOfClientThreadsPerReceiver number of threads that can answer calls.
+	 * @param maxSizeOfWaitingList number of pending calls
+	 * @param listeningPort ports on which the server exchange the information with the clients
+	 * @param internalPorts the backdoor port followed by the garbage collection port (0 for random port selection)
+	 * @param key a security token to ensure the client is really a known user
+	 */
+	public ServerConfiguration(int numberOfClientThreadsPerReceiver, int maxSizeOfWaitingList, int[] listeningPorts, int[] internalPorts, int key) {
+		this(listeningPorts, internalPorts, numberOfClientThreadsPerReceiver, maxSizeOfWaitingList, false, key, null);
+	}
+
+	
+	/**
+	 * General constructor. <br>
+	 * <br>
+	 * A private server is a server that is instantiated by the client and shut down when the client closes or 
+	 * the connection is lost. It also needs a security key and a working directory. The key is written in a 
+	 * file and retrieved by the client for a security check. <br>
+	 * 
+	 * <br>
+	 * 
+	 * A public server is a server that is already running and does not shut down when the client closes the 
+	 * connection or the connection is lost. It is instantiated manually and usually not from the client environment.
+	 * 
+	 * @param listiningPorts the ports to which the ServerSocket will listen (0 for random port selection)
+	 * @param internalPorts the backdoor port followed by the garbage collection port (0 for random port selection)
+	 * @param numberOfClientThreadsPerReceiver
+	 * @param maxSizeOfWaitingList
+	 * @param isPrivateServer true for a private server or false otherwise
+	 * @param key a security token to ensure the client is really a known user
+	 * @param wd a string representing the working directory. If it is invalid, then Java uses the temporary directory as
+	 * specified in the property 
+	 */
+	private ServerConfiguration(int[] listeningPorts, 
+			int[] internalPorts, 
+			int numberOfClientThreadsPerReceiver, 
+			int maxSizeOfWaitingList, 
+			boolean isPrivateServer, 
+			int key, 
+			String wd) {
+		this.isPrivateServer = isPrivateServer;
+		if (numberOfClientThreadsPerReceiver < 0 || numberOfClientThreadsPerReceiver > 10) {
+			throw new InvalidParameterException("Number of client threads should be between 1 and 10!"); 
+		} else {
+			this.numberOfClientThreadsPerReceiver = numberOfClientThreadsPerReceiver;
+		}
+		if (listeningPorts == null) {
+			throw new InvalidParameterException("The listeningPorts argument cannot be set to null!");
+		}
+		for (int port : listeningPorts) {
+			checkPort(port, "listening");
+		}
+		this.listiningPorts = listeningPorts;
+		for (int port : internalPorts) {
+			checkPort(port, "internal");
+		}
+		this.internalPorts = internalPorts;
+		if (maxSizeOfWaitingList < 0) {
+			this.maxSizeOfWaitingList = 0;
+		} else {
+			this.maxSizeOfWaitingList = maxSizeOfWaitingList;
+		}
+		this.key = key;
+		if (isPrivateServer()) {
+			if (wd == null || !new File(wd).exists()) {
+				this.wd = System.getProperty("java.io.tmpdir");
+			} else {
+				this.wd = wd;
+			}
+		} else {
+			this.wd = null;
+		}
+	}
+	
+	/**
+	 * Configuration for private servers.
+	 * 
+	 * @param listiningPorts the ports to which the ServerSocket will listen (0 for random port selection)
+	 * @param internalPorts the backdoor port followed by the garbage collection port (0 for random port selection)
+	 * @param key a security token to ensure the client is really a known user
+	 * @param wd a string representing the working directory. If it is invalid, then Java uses the temporary directory as
+	 * specified in the property 
+	 */
+	public ServerConfiguration(int[] listiningPorts, int[] internalPorts, int key, String wd) {
+		this(listiningPorts, internalPorts, 1, 0, true, key, wd);
+	}
+	
+	boolean isPrivateServer() {return isPrivateServer;}
+
+	protected List<ServerSocket> createServerSockets() throws IOException {
+		List<ServerSocket> sockets = new ArrayList<ServerSocket>();
+		for (int port : listiningPorts) {
+			sockets.add(new ServerSocket(port));
+		}
+		return sockets;
+	}
+	
+	private void checkPort(int port, String portType) {
+		if (port != 0 && (port < MIN_PORT || port > MAX_PORT)) {
+			throw new InvalidParameterException("The " + portType + " port must be between " + MIN_PORT + " and " + MAX_PORT + " (or can be 0 for a random port)!");
+		}			
+	}
+	
+	
+}	
